541afb8ccfb8bef1af3378ccef5fc3543b3ef52a
gitluky <jluk87@gmail.com>
2017-09-09 23:09:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musician, instrument){
   var band = new Array()
   for (var i = 0; i < musician.length; i++){
-    var musicianAndInstrument = musician[i] + ' plays ' + 'instrument[i]'
+    var musicianAndInstrument = musician[i] + ' plays ' + instrument[i]
     band.push(musicianAndInstrument)
   }
   return band
